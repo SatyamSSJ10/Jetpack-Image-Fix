13f8549310c91a999aea00f5fd2f683398757683
SatyamSSJ10 <60954779+SatyamSSJ10@users.noreply.github.com>
2023-05-10 19:48:09 +0530

    Create script.js
    Put this as HTML widget

---
+++ script.js
@@ -0,0 +1,32 @@
+<style>
+  #fix-images-btn {
+    position: fixed;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px 20px;
+    border-radius: 10px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+</style>
+<button id="fix-images-btn">Fix broken images</button>
+
+<script>
+  document.getElementById("fix-images-btn").addEventListener("click", function () {
+    const images = document.querySelectorAll("div.page-break img"); // get all images under div with class page-break
+    for (let i = 0; i < images.length; i++) {
+      images[i].onerror = function () { // check if the image fails to load
+        const imgUrl = images[i].src;
+        const newImgUrl = imgUrl.replace("?ssl=1", ""); // remove the "?ssl=1" parameter from the image URL
+        images[i].src = newImgUrl; // set the new image URL
+        images[i].addEventListener("load", function () { // reload the image after correcting the link
+          images[i].src = newImgUrl;
+        })
+      }
+    }
+    alert("IF THE IMAGE DOESN'T APPEAR PLEASE RIGHT CLICK THE IMAGE ICON AND RELOAD IMAGE.");
+  });
+</script>
